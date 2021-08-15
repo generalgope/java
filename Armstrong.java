7b06939cf40c58fde61dcdd5e9490e88c0bfc52f
generalgope <82454512+generalgope@users.noreply.github.com>
2021-08-15 20:09:53 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Armstrong {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+	
+		int num, temp, count, digit, sum = 0;
+		
+		System.out.print("Enter a number: ");
+		num = scan.nextInt();
+		temp = num;
+		
+		for (count = 0; temp > 0; count++) {
+			temp /= 10; 	//temp = temp / 10
+		}
+		
+		temp = num;
+		while(temp > 0) {
+			digit = temp % 10;
+			temp /= 10;
+			System.out.println(temp);
+			sum += Math.pow(digit, count);
+		}
+		
+		if (num == sum) {
+			System.out.println(num + " is an Armstrong number.");
+		}
+		else {
+			System.out.println(num + " is not an Armstrong number.");
+		}
+	}
+}
+
+
