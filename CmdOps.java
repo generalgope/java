b42af7fa5aa0f63292d6a81543a76bdf873db3b1
generalgope <82454512+generalgope@users.noreply.github.com>
2021-08-16 22:17:04 +0530

    Add files via upload

---
+++ CmdOps.java
@@ -0,0 +1,38 @@
+class CmdOps {
+	public static void main (String args[]) {
+		int i, sum = 0;
+		double doubleSum, avg;
+		int num = Integer.parseInt(args[1]);
+		
+		switch (args[0]) {
+			case "odd":
+				for (i = 0; i <= num; i++) {
+					if ((i %2 ) != 0)
+						System.out.print(i + " ");
+				}
+				System.out.println();
+				break;			
+			case "even":
+				for (i = 0; i <= num; i++) {
+					if ((i % 2) == 0)
+						System.out.print(i + " ");
+				}
+				System.out.println();
+				break;	
+			case "sum":
+				for (i = 0; i <= num; i++) {
+					sum += i;
+				}
+				System.out.println("Sum = " + sum);
+				break;	
+			case "avg":
+				for (i = 0; i <= num; i++) {
+					sum += i;
+				}
+				doubleSum = sum;
+				avg = doubleSum / num;
+				System.out.println("Average = " + avg);
+				break;
+		}
+	}
+}
