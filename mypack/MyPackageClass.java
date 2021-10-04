ed8884d84c31dd984d5999148eb6f937592d0d64
mp <82454512+generalgope@users.noreply.github.com>
2021-10-05 01:18:42 +0530

    Create MyPackageClass.java

---
+++ mypack/MyPackageClass.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class MyPackageClass {
+		int favNum = 7;
+		public void show() {
+			System.out.println("My favorite number is " + favNum);
+		}
+}
+
+/*
+javac MyPackageClass.java 
+javac -d </Destop> <.> MyPackageClass.java
+*/
