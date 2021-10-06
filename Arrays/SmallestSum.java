1010833b4ec972268a1aef0f0fa437b57c0d1ddc
mp <82454512+generalgope@users.noreply.github.com>
2021-10-06 23:04:30 +0530

    Update SmallestSum.java

--- Arrays/SmallestSum.java
+++ Arrays/SmallestSum.java
@@ -1,4 +1,6 @@
-class SmallestSum {
+// Work in progress...
+
+class SmallestSumPair {
 	public static void main(String[] args) {
 		int[] nums1 = {1, 7, 11};
 		int[] nums2 = {2, 4, 6};
@@ -7,13 +9,14 @@ class SmallestSum {
 		int[] sums = new int[size/2];
 		int k = 3;
 		
+		// Step 1: Make all possible pairs and store them in pairs[]
 		int m = 0;
 		for (int i = 0; i < nums1.length; i++) {
 			for (int j = 0; j < nums2.length; j++) {
-					pairs[m] = nums1[i]; 
-					m = m + 1;
-					pairs[m] = nums2[j];
-					m = m + 1;
+				pairs[m] = nums1[i]; 
+				m = m + 1;
+				pairs[m] = nums2[j];
+				m = m + 1;
 			}
 		}
 		
@@ -21,6 +24,10 @@ class SmallestSum {
 			System.out.print(pairs[x] + " ");
 		}
 		System.out.println("\n");
+		
+		// Step 2: Add each element in the pair and store the sum in sum[]
+		// Step 3: Find the smallest value (sum) in sum[]
+		// Step 4: Output the pair (from pairs[]) which has the smallest sum
 		
 		/*
 		for (int p = 0; p < sums.length; p++) {
