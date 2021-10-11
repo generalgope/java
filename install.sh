317af6c67769801b17c311e048acf668a465e4dd
generalgope <patel.mihir2024@gmail.com>
2021-10-11 17:37:23 +0530

    added install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update 
+echo [1/3] Successfully updated package list
+sudo apt install default-jre
+echo [2/3] Successfully installed Java Runtime Environment
+sudo apt install default-jdk
+echo [3/3] Successfully installed Java Development Kit]
+echo All dependencies installed!
